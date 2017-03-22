05197f20bc8634afd0fb3a8afe9d049a4637439a
charmi khambhati <incharmi.ck@gmail.com>
2017-03-22 18:10:05 +0530

    Update imageView.php

--- imageView.php
+++ imageView.php
@@ -23,15 +23,27 @@
 	
 	include("connection.php");
 	
-	echo $_SESSION['email']." its from me ".$_SESSION['id'];
+	echo $_SESSION['email']." its from me ".$_SESSION['id']."\n";
 	
-	$sql= "select * from image_table order by img_id desc";
+	$sql= "select * from image_table,users order by img_id desc";
 	$result=mysqli_query($conn, $sql);
+	$result=mysqli_query($conn, $sql);
+	echo "\n";
 	while($row=mysqli_fetch_array($result))
 	{
 	?>
-	<br>
-		<img src="uploads/<?php echo $row['img_name']?>" width="20%" height="auto">
+	<table style="width:100%">
+		
+		<tr><td><img src="uploads/<?php echo $row['img_name']?>" width="20%" height="auto"></td></tr>
+		 <tr><td><?php echo $row['first_name']." ".$row['last_name'];?> </td></tr>
+		 <tr><td><?php 		  echo $row['name'];?> </td></tr>
+		 <tr><td><?php 		  echo $row['type'];?></td></tr>
+		 <tr><td><?php 		echo $row['used_for'];?> </td></tr>
+		 <tr><td><?php 	  echo $row['price'];?> </td></tr>
+		 <tr><td><?php 	  echo $row['description'];?> </td></tr>
+		 
+		 </table>
+		
 	<?php
 	}
 
