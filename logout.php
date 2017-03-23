c5291dcd302605073d37a6ef3a44ae8979057a53
charmi khambhati <incharmi.ck@gmail.com>
2017-03-23 22:23:42 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,7 +5,8 @@ include_once 'gpConfig.php';
 //Unset token and user data from session
 unset($_SESSION['token']);
 unset($_SESSION['userData']);
-
+unset($_SESSION['email']);
+unset($_SESSION['id']);
 //Reset OAuth access token
 $gClient->revokeToken();
 
