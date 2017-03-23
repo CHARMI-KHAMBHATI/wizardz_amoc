ee1314bcb6bae34650e9228e68608f89e4965cbb
charmi khambhati <incharmi.ck@gmail.com>
2017-03-23 18:45:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -43,21 +43,7 @@ if ($gClient->getAccessToken()) {
 	
     header("location:imageView.php");
 	
-    //Render facebook profile data
-    if(!empty($userData)){
-        $output = '<h1>Google+ Profile Details </h1>';
-        $output .= '<img src="'.$userData['picture'].'" width="300" height="220">';
-        $output .= '<br/>Google ID : ' . $userData['oauth_uid'];
-        $output .= '<br/>Name : ' . $userData['first_name'].' '.$userData['last_name'];
-        $output .= '<br/>Email : ' . $userData['email'];
-        $output .= '<br/>Gender : ' . $userData['gender'];
-        $output .= '<br/>Locale : ' . $userData['locale'];
-        $output .= '<br/>Logged in with : Google';
-        $output .= '<br/><a href="'.$userData['link'].'" target="_blank">Click to Visit Google+ Page</a>';
-        $output .= '<br/>Logout from <a href="logout.php">Google</a>'; 
-    }else{
-        $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
-    }
+    
 } else {
     $authUrl = $gClient->createAuthUrl();
     $output = '<a href="'.filter_var($authUrl, FILTER_SANITIZE_URL).'"><img src="images/glogin.png" alt=""/></a>';
