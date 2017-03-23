736eda753ba6122cdb9aaadf8a88319d3c1413fb
Ayushi Solanki <ayupsol1@gmail.com>
2017-03-23 21:49:02 +0530

    Update imageView.php

--- imageView.php
+++ imageView.php
@@ -3,20 +3,30 @@
 
 <html>
 	<head>
-		
-		<title>
-		My upload
-		</title>
+	<title>QuickBucket</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device.width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="./styles/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/style_home.css">
+	<script src="./script/jquery-3.2.0.min.js"></script>
+	<script src="./script/bootstrap.min.js"></script>
 	</head>
 	
 	<body>
+	<div class="navigation">
+	<div class="container">
+	<div class="row">
+	<ul>
+	<li class="col-xs-4 col-md-2"><a href="./imageView.php">Home</a></li>
+	<li class="col-xs-4 col-md-2"><a href="Upload_file.html">Upload</a></li>
+	<li class="col-xs-4 col-md-2" id="logout"><a href="./logout.php">Logout</a></li>
+	</ul>
+	</div>	
+
+	</div>
+	</div>
 	
-	Logout from <a href="logout.php">Google</a>
-	<br>
-	  <a href="Upload_file.html">upload</a>
 	
-	<br>
-	<table>
 	
 	<?php
 	include_once 'gpConfig.php';
@@ -29,28 +39,26 @@
 	$result=mysqli_query($conn, $sql);
 	$result=mysqli_query($conn, $sql);
 	echo "\n";
-	
-	echo '<script> var i=0;</script>';
-	
 	while($row=mysqli_fetch_array($result))
 	{
 	?>
-	<div class="contents">
-	<img src="uploads/<?php echo $row['img_name'] ?>" width="20%" height="auto" >
-	
-	<p> <?php echo $row['name']; ?> </p>
-	<p><?php echo $row['used_for']; ?> </p>
-	<p><?php echo $row['price']; ?> </p>
-	</div>
-	<?php echo '<script>
-	ele.className=i.toString();
-	i++;</script>' ?>
+	<table style="width:100%">
+		
+		<tr><td><img src="uploads/<?php echo $row['img_name']?>" width="20%" height="auto"></td></tr>
+		 <tr><td><?php echo $row['first_name']." ".$row['last_name'];?> </td></tr>
+		 <tr><td><?php 		  echo $row['name'];?> </td></tr>
+		 <tr><td><?php 		  echo $row['type'];?></td></tr>
+		 <tr><td><?php 		echo $row['used_for'];?> </td></tr>
+		 <tr><td><?php 	  echo $row['price'];?> </td></tr>
+		 <tr><td><?php 	  echo $row['description'];?> </td></tr>
+		 
+		 </table>
+		
 	<?php
 	}
-		
+
 	?>
-</table>
+
 </body>
 
 </html>
-
