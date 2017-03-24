fcec42f0df7e4d204b6bceea74af89231a6848e1
Ayushi Solanki <ayupsol1@gmail.com>
2017-03-24 19:17:45 +0530

    Add files via upload

---
+++ description.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+	<title>QuickBucket</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device.width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="./css/style_home.css">
+	<link rel="stylesheet" type="text/css" href="./styles/bootstrap.min.css">
+	<script src="./script/jquery-3.2.0.min.js"></script>
+	<script src="./script/bootstrap.min.js"></script>
+	</head>
+	
+	<body>
+	<div class="navigation">
+	<div class="container">
+	<div class="row">
+	<ul>
+	<li class="col-xs-4 col-md-2"><a href="./imageView.php">Home</a></li>
+	<li class="col-xs-4 col-md-2"><a href="Upload_file.html">Upload</a></li>
+	<li class="col-xs-4 col-md-2" id="logout"><a href="./logout.php">Logout</a></li>
+	</ul>
+	</div>	
+
+	</div>
+	</div>
+	
+	</body>
+</html>
