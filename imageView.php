be092b9a941a316481d89c8eead72315b006aee7
Ayushi Solanki <ayupsol1@gmail.com>
2017-03-24 19:18:21 +0530

    Update imageView.php

--- imageView.php
+++ imageView.php
@@ -3,19 +3,28 @@
 
 <html>
 	<head>
-		
-		<title>
-		My upload
-		</title>
+	<title>QuickBucket</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device.width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="./css/style_home.css">
+	<link rel="stylesheet" type="text/css" href="./styles/bootstrap.min.css">
+	<script src="./script/jquery-3.2.0.min.js"></script>
+	<script src="./script/bootstrap.min.js"></script>
 	</head>
 	
 	<body>
-	
-	Logout from <a href="logout.php">Google</a>
-	<br>
-	  <a href="Upload_file.html">upload</a>
-	
-	<br>
+	<div class="navigation">
+	<div class="container">
+	<div class="row">
+	<ul>
+	<li class="col-xs-4 col-md-2"><a href="./imageView.php">Home</a></li>
+	<li class="col-xs-4 col-md-2"><a href="Upload_file.html">Upload</a></li>
+	<li class="col-xs-4 col-md-2" id="logout"><a href="./logout.php">Logout</a></li>
+	</ul>
+	</div>	
+
+	</div>
+	</div>
 	
 	
 	<?php
@@ -23,32 +32,30 @@
 	
 	include("connection.php");
 	
-	echo $_SESSION['email']." its from me ".$_SESSION['id']."\n";
 	
-	$sql= "select * from image_table,users order by img_id desc";
+	$sql= "select * from image_table order by img_id desc";
 	$result=mysqli_query($conn, $sql);
 	$result=mysqli_query($conn, $sql);
 	echo "\n";
+	
 	while($row=mysqli_fetch_array($result))
 	{
 	?>
-	<table style="width:100%">
+	
+	<div class="col-xs-6 col-md-4" id="contents">
 		
-		<tr><td><img src="uploads/<?php echo $row['img_name']?>" width="20%" height="auto"></td></tr>
-		 <tr><td><?php echo $row['first_name']." ".$row['last_name'];?> </td></tr>
-		 <tr><td><?php 		  echo $row['name'];?> </td></tr>
-		 <tr><td><?php 		  echo $row['type'];?></td></tr>
-		 <tr><td><?php 		echo $row['used_for'];?> </td></tr>
-		 <tr><td><?php 	  echo $row['price'];?> </td></tr>
-		 <tr><td><?php 	  echo $row['description'];?> </td></tr>
-		 
-		 </table>
-		
+		<a href="./description.php"><img src="uploads/<?php echo $row['img_name'] ?>"> </a>
+		 <div id="contents_description">
+		 <p>Name: <span class="description"><?php echo $row['name']; ?></span> </p>
+		 <p>Useful for: <span class="description"><?php echo $row['used_for']; ?></span> </p>
+		 <p>Price: Rs.<span class="description"><?php echo $row['price']; ?></span> </p>
+		 </div>
+	</div>
+
 	<?php
 	}
 
 	?>
-
 </body>
 
 </html>
