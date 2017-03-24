52492158437625a981a53a0a7c88130de330a019
charmi khambhati <incharmi.ck@gmail.com>
2017-03-24 21:04:45 +0530

    Update imageView.php

--- imageView.php
+++ imageView.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 
 <html>
@@ -19,6 +18,7 @@
 	<ul>
 	<li class="col-xs-4 col-md-2"><a href="./imageView.php">Home</a></li>
 	<li class="col-xs-4 col-md-2"><a href="Upload_file.html">Upload</a></li>
+	<li class="col-xs-4 col-md-2"><a href="myUploads.php">MyUploads</a></li>
 	<li class="col-xs-4 col-md-2" id="logout"><a href="./logout.php">Logout</a></li>
 	</ul>
 	</div>	
@@ -53,7 +53,6 @@
 
 	<?php
 	}
-
 	?>
 </body>
 
