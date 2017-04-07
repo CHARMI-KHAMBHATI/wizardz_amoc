5210a68da38236a0bd038f44d4545e12b793652a
Pooja Suthar <poojasuthar65@gmail.com>
2017-04-07 20:06:31 +0530

    Update editComment.php

--- editComment.php
+++ editComment.php
@@ -5,26 +5,15 @@
 include_once 'gpConfig.php';
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>EditComment</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
+
 <?php
-	$uid=$_POST['uid'];
-	$cid=$_POST['cid'];
-	$cdate=$_POST['cdate'];
-	$message=$_POST['message'];
-	
-	echo "<form method='POST' action='".editComment($conn)."'>
-		<input type='hidden' name='cid' value='".$cid."'>
-		<input type='hidden' name='uid' value='".$uid."'>
-		<input type='hidden' name='cdate' value='".$cdate."'>
-		<textarea name='message' >".$message."</textarea>
-		<button type='submit' name='commentSubmit' >Edit</button>
-	</form>";
+	if(isset($_POST['submit'])){
+			$cid=$_POST['cid'];
+			$cdate=$_POST['cdate'];
+			$message=$_POST['message'];
+			
+			$sql="UPDATE commentsection SET message='$message' WHERE cid='$cid'";
+			$result=$conn->query($sql);
+			header("Location:commentpage.php");
+		}
 ?>
-</body>
-</html>
